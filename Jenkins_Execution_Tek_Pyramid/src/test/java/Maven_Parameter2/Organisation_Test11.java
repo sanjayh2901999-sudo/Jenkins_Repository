1c8dd924421999d46600104c3553eb165b256ca0
sanjayh2901999-sudo <sanjayh2901999@gmail.com>
2026-05-05 09:29:15 +0530

    I am pushing a new project file which i will be exevcuting via jenkins

---
+++ Jenkins_Execution_Tek_Pyramid/src/test/java/Maven_Parameter2/Organisation_Test11.java
@@ -0,0 +1,17 @@
+package Maven_Parameter2;
+
+import org.testng.annotations.Test;
+
+public class Organisation_Test11
+{
+@Test
+public void CreateOrganisationTest()
+{
+	System.out.println("Execute CreateOrganisationTest");
+}
+@Test
+public void ModifyOrganisationTest()
+{
+	System.out.println("Execute ModifyOrganisationTest");
+}
+}
